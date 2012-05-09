556ded5103c74486a6b49bb2b93e0de743e82c1e
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-05-09 16:22:23 +0000

    lots of changes (multi domain)

--- server.js
+++ server.js
@@ -9,22 +9,29 @@
 	app.set('view engine', 'ejs');
 	
 	app.use("/static", express.static(__dirname + '/static'));
+    app.use(app.router); // avoid static treatment
 	
-	app.get('/', function(req, res){
-		
-		http.request(
-			{
-				host: 'www.colorfyit.com',
-				path: '/api/swatches/list.json?url=www.viadeo.com&discover=true'
-			},
-			function(response) {
-				var str = '';
-				response.on('data', function (chunk) { str += chunk; });
-				response.on('end', function () {
-					res.render('colors', { datas : JSON.parse(str).colors });
-				});
-			}
-		).end();
+	app.get('/:target', function(req, res){
+        
+        var target =  req.params.target;
+        
+        if(target){
+        
+    		http.request(
+    			{
+    				host: 'www.colorfyit.com',
+    				path: '/api/swatches/list.json?url=www.' + target + '.com&discover=true'
+    			},
+    			function(response) {
+    				var str = '';
+    				response.on('data', function (chunk) { str += chunk; });
+    				response.on('end', function () {
+    					res.render('colors', { datas : JSON.parse(str).colors, target : target });
+    				});
+    			}
+    		).end();
+        
+        }
 	
 	});
 		
