ba3e2853923cc0c33b82ee3b922850a4c2641ddf
Sylvain Faucherand <syl.faucherand@gmail.com>
2014-03-24 15:49:10 +0100

    Cleaning uneeded dependencies and adding missing ones in pkg.json

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Viadeo Colors distribution
 ==========================
 
-Testing cloud9 IDE and heroku for demployement. See results here : http://colors-distribution-viadeo.herokuapp.com/
+http://colors-distribution-viadeo.herokuapp.com/
--- server.js
+++ server.js
@@ -1,46 +1,46 @@
    
-	var 
-		http = require('http'),
-		express = require('express'),
-		app = express.createServer(),
-		ejs = require('ejs')
-	;
-	
-	app.set('view engine', 'ejs');
-	
-	app.use("/static", express.static(__dirname + '/static'));
+    var 
+        http = require('http'),
+        express = require('express'),
+        app = express.createServer(),
+        ejs = require('ejs')
+    ;
+    
+    app.set('view engine', 'ejs');
+    
+    app.use('/static', express.static(__dirname + '/static'));
     app.use(app.router); // avoid static treatment
-	
+    
     app.get('/', function(req, res){
         var target =  "viadeo";
         render(target, res);
     });
     
-	app.get('/:target', function(req, res){
+    app.get('/:target', function(req, res){
         var target =  req.params.target;
         if(target){ render(target, res); }
-	});
+    });
     
     function render(target, res){
        
        if(target){
         
-        	http.request(
-    			{
-    				host: 'www.colorfyit.com',
-    				path: '/api/swatches/list.json?url=www.' + target + '.com&discover=true'
-    			},
-    			function(response) {
-    				var str = '';
-    				response.on('data', function (chunk) { str += chunk; });
-    				response.on('end', function () {
-    					res.render('colors', { datas : JSON.parse(str).colors, target : target });
-    				});
-    			}
-    		).end();
+            http.request(
+                {
+                    host: 'www.colorfyit.com',
+                    path: '/api/swatches/list.json?url=www.' + target + '.com&discover=true'
+                },
+                function(response) {
+                    var str = '';
+                    response.on('data', function (chunk) { str += chunk; });
+                    response.on('end', function () {
+                        res.render('colors', { datas : JSON.parse(str).colors, target : target });
+                    });
+                }
+            ).end();
         
         }
        
     }
-		
-	app.listen(process.env.PORT);
+        
+    app.listen(process.env.PORT || 9000);
